25390cae595580e15eb89a0510e15dda313b7022
hirotodukan <57314296+hirotodukan@users.noreply.github.com>
2019-11-15 19:22:44 +0900

    sach.vol2

--- app/assets/javascripts/massege.js
+++ app/assets/javascripts/massege.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildMessage(message){
-    if ( message.image ) {
+    if ( message.image.url ) {
       var html = `<div class="contents__main__messages__data">
                     <div class="contents__main__messages__data__info">
                       <div class="contents__main__messages__data__info__talker">
@@ -16,7 +16,7 @@ $(function(){
                           
                         </p>
                       </div>
-                      <img src=${message.image} >
+                        <img src=${message.image} >
                   </div>`
       return html;
     } else{
@@ -63,5 +63,77 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   })
+  var reloadMessages = function() {
+    last_message_id = $('.messages:last').data('id');
+
+    var buildMessageHTML = function(message) {
+      if (message.content && message.image.url) {
+        //data-idが反映されるようにしている
+        var html = `<div class="message" data-id=` + message.id + `>` +
+          `<div class="upper-message">` +
+            `<div class="upper-message__user-name">` +
+              message.user_name +
+            `</div>` +
+            `<div class="upper-message__date">` +
+              message.created_at +
+            `</div>` +
+          `</div>` +
+          `<div class="lower-message">` +
+            `<p class="lower-message__content">` +
+              message.content +
+            `</p>` +
+            `<img src="` + message.image.url + `" class="lower-message__image" >` +
+          `</div>` +
+        `</div>`
+      } else if (message.content) {
+        //同様に、data-idが反映されるようにしている
+        var html = `<div class="message" data-id=` + message.id + `>` +
+          `<div class="upper-message">` +
+            `<div class="upper-message__user-name">` +
+              message.user_name +
+            `</div>` +
+            `<div class="upper-message__date">` +
+              message.created_at +
+            `</div>` +
+          `</div>` +
+          `<div class="lower-message">` +
+            `<p class="lower-message__content">` +
+              message.content +
+            `</p>` +
+          `</div>` +
+        `</div>`
+      } else if (message.image.url) {
+        //同様に、data-idが反映されるようにしている
+        var html = `<div class="message" data-id=` + message.id + `>` +
+          `<div class="upper-message">` +
+            `<div class="upper-message__user-name">` +
+              message.user_name +
+            `</div>` +
+            `<div class="upper-message__date">` +
+              message.created_at +
+            `</div>` +
+          `</div>` +
+          `<div class="lower-message">` +
+            `<img src="` + message.image.url + `" class="lower-message__image" >` +
+          `</div>` +
+        `</div>`
+      };
+      return html;
+    };
+
+    $.ajax({
+      url: groups/message.id/messages,
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+      $('.contents__main__messages').animate({scrollTop: $('.contents__main__messages')[0].scrollHeight}, 'fast');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
 });
 
